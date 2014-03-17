28fcabcc0c710266d58870de533249ac61db0cc0
Derrick Reimer <derrickreimer@gmail.com>
2014-03-16 21:16:30 -0700

    Use forwardable for delegation

--- lib/quack/value.rb
+++ lib/quack/value.rb
@@ -1,19 +1,17 @@
+require "forwardable"
+
 module Quack
   class Value
+    extend Forwardable
     attr_reader :value, :type_class, :type_instance
+    
+    delegate :to_coerced => :type_instance
+    delegate :to_s => :type_instance
 
     def initialize(value)
       @value = value
       @type_class = Quack::Types.select { |t| t.matches?(value) }.first
       @type_instance = type_class.new(value)
-    end
-
-    def to_coerced
-      type_instance.to_coerced
-    end
-
-    def to_s
-      type_instance.to_s
     end
 
     def type_matches?(other)
