45da93fa349a2d6e77cdbf414a28e20f97b94fef
Tim Breitkreutz <tim@sbrew.com>
2016-12-26 16:02:30 -0800

    tighten up time detection

--- spec/quack/guesser_spec.rb
+++ spec/quack/guesser_spec.rb
@@ -4,7 +4,7 @@ describe Quack::Guesser do
   describe "given a string integer" do
     let(:value) { "123" }
     let(:subject) { Quack::Guesser.new(value) }
-    
+
     it "should have Integer type" do
       subject.guess.class.must_equal(Quack::Types::Integer)
     end
@@ -79,6 +79,33 @@ describe Quack::Guesser do
 
     it "should have Time type" do
       subject.guess.class.must_equal(Quack::Types::Time)
+    end
+  end
+
+  describe "given a padded ISO 8061 UTC date" do
+    let(:value) { "     2014-03-22T03:00:00Z " }
+    let(:subject) { Quack::Guesser.new(value) }
+
+    it "should have Time type" do
+      subject.guess.class.must_equal(Quack::Types::Time)
+    end
+  end
+
+  describe "given an embedded ISO 8061 UTC date" do
+    let(:value) { "something awesome will happen 2014-03-22T03:00:00Z or thereabouts" }
+    let(:subject) { Quack::Guesser.new(value) }
+
+    it "should have String type" do
+      subject.guess.class.must_equal(Quack::Types::String)
+    end
+  end
+
+  describe "given an embedded simple date" do
+    let(:value) { "something awesome will happen 2014-03-22 or thereabouts" }
+    let(:subject) { Quack::Guesser.new(value) }
+
+    it "should have String type" do
+      subject.guess.class.must_equal(Quack::Types::String)
     end
   end
 
